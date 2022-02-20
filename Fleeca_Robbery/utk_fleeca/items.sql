0677ac41a90a4245d0e41beb9e45fa8722abd01f
Mosterdgas <97600291+Mosterdgas@users.noreply.github.com>
2022-02-21 00:43:10 +0100

    Add files via upload

---
+++ Fleeca_Robbery/utk_fleeca/items.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+    ('id_card_f', 'Gehackte Toegangs Kaart', 1, 3, 1),
+    ('secure_card', 'Beveiligd ID Kaart', 1, 3, 1);
